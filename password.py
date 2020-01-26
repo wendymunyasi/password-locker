e247fbbeadf631092ab4471ef4377dd79101d323
Wendy Munyasi <wendymunyasi@gmail.com>
2020-01-26 17:47:33 +0300

    Add save_user method.

--- password.py
+++ password.py
@@ -2,10 +2,9 @@ class User:
     """
     Class that generates new instances of the user
     """
-    user_list = [] # Empty user list
-    
-    def __init__(self,first_name,last_name,number,email):
-        
+    user_list = []  # Empty user list
+
+    def __init__(self, first_name, last_name, number, email):
         """
         __init__ method that helps us define properties for our objects.
         """
@@ -13,4 +12,12 @@ class User:
         self.last_name = last_name
         self.phone_number = number
         self.email = email
+
+    def save_user(self):
+    
+        """
+        save_user method saves user objects into user_list
+        """
+
+        User.user_list.append(self)
 
