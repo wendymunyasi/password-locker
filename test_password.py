2168df5da11ae40dd736ba472a343eb845f22e3f
Wendy Munyasi <wendymunyasi@gmail.com>
2020-01-26 17:41:02 +0300

    Add test to test if a new user is saved once created.

--- test_password.py
+++ test_password.py
@@ -16,13 +16,21 @@ class TestUser(unittest.TestCase):
 
     def test_instance(self):
         '''
-        test_init test case to test if the object is initialized properly
+        To test if the object is initialized properly
         '''
 
         self.assertEqual(self.new_user.first_name, "Wendy")
         self.assertEqual(self.new_user.last_name, "Munyasi")
         self.assertEqual(self.new_user.phone_number, "0707240068")
         self.assertEqual(self.new_user.email, "wendymunyasi@gmail.com.com")
+
+    def test_save_user(self):
+        '''
+        To test if the user object is saved into
+         the user list
+        '''
+        self.new_user.save_user()  # saving the new contact
+        self.assertEqual(len(User.user_list), 1)
 
 
 if __name__ == '__main__':
