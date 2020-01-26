a1d66bd74c9bce08fb62f8e35acbbf15cd57a692
Wendy Munyasi <wendymunyasi@gmail.com>
2020-01-26 18:06:37 +0300

    Add a test to delete user in the test_password.py file and add a code in the password.py to make the test pass.

--- password.py
+++ password.py
@@ -14,10 +14,14 @@ class User:
         self.email = email
 
     def save_user(self):
-    
         """
         save_user method saves user objects into user_list
         """
-
         User.user_list.append(self)
 
+    def delete_user(self):
+        '''
+        delete_user method deletes a saved user from the user_list
+        '''
+
+        User.user_list.remove(self)
--- test_password.py
+++ test_password.py
@@ -48,6 +48,18 @@ class TestUser(unittest.TestCase):
         test_user.save_user()
         self.assertEqual(len(User.user_list), 2)
 
+    def test_delete_user(self):
+        '''
+        test_delete_user to test if we can remove a user from our user list
+        '''
+        self.new_user.save_user()
+        test_user = User("Test", "User", "0712345678",
+                         "test@user.com")  # new user
+        test_user.save_user()
+
+        self.new_user.delete_user()  # Deleting a contact object
+        self.assertEqual(len(User.user_list), 1)
+
 
 if __name__ == '__main__':
     unittest.main()
