618deeb4a60d09ba0672738a3652d787512c87b6
Wendy Munyasi <wendymunyasi@gmail.com>
2020-01-26 17:29:03 +0300

    Create an empty user_list and define the __init__function in the class.

--- password.py
+++ password.py
@@ -2,5 +2,15 @@ class User:
     """
     Class that generates new instances of the user
     """
-    pass
+    user_list = [] # Empty user list
+    
+    def __init__(self,first_name,last_name,number,email):
+        
+        """
+        __init__ method that helps us define properties for our objects.
+        """
+        self.first_name = first_name
+        self.last_name = last_name
+        self.phone_number = number
+        self.email = email
 
